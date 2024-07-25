e4970b19860899caf0aa21ed0f0a714a1aeef3f6
Serpyne <robbingchew@gmail.com>
2024-07-26 00:55:36 +1000

    Delete test.py

--- project/test.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import pymysql
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://serpyne:suspiciousMammal?!3572@storage-saver.cnk0kagqe046.ap-southeast-2.rds.amazonaws.com/countpool?charset=utf8mb4'
-db = SQLAlchemy(app)
